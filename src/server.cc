af1e2fb0d758bd06be1a588b94a7fcf75b7d63c1
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-24 14:57:23 -0700

    add rpc

--- src/server.cc
+++ src/server.cc
@@ -8,12 +8,12 @@ namespace tl = thallium;
 std::function<void(const tl::request&, int&, int&, std::vector<int>&, std::vector<int64_t>&, std::vector<int64_t>&)> sum =
         [*](const tl::request& req, int& x, int& y, std::vector<int>& types, std::vector<int64_t>& data_buff_sizes, std::vector<int64_t>& offset_buff_sizes) {        
         req.respond(x+y);
-    }
+    };
 
 std::function<void(const tl::request&, int&, int&)> sum_light =
         [*](const tl::request& req, int& x, int& y) {        
         req.respond(x+y);
-    }
+    };
 
 
 int main(int argc, char** argv) {
