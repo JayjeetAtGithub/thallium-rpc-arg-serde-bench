0934863404f28e4134e65e7ea41dd98673a3eb3e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-24 14:01:57 -0700

    initial commit

---
+++ src/server.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thallium.hpp>
+
+namespace tl = thallium;
+
+void sum(const tl::request& req, int x, int y) {
+    std::cout << "Computing " << x << "+" << y << std::endl;
+    req.respond(x+y);
+}
+
+int main(int argc, char** argv) {
+
+    tl::engine myEngine("tcp://127.0.0.1:1234", THALLIUM_SERVER_MODE);
+    std::cout << "Server running at address " << myEngine.self() << std::endl;
+    myEngine.define("sum", sum);
+
+    return 0;
+}
+#include <iostream>
+#include <thallium.hpp>
+
+namespace tl = thallium;
+
+void sum(const tl::request& req, int x, int y) {
+    std::cout << "Computing " << x << "+" << y << std::endl;
+    req.respond(x+y);
+}
+
+int main(int argc, char** argv) {
+
+    tl::engine myEngine("tcp://127.0.0.1:1234", THALLIUM_SERVER_MODE);
+    std::cout << "Server running at address " << myEngine.self() << std::endl;
+    myEngine.define("sum", sum);
+
+    return 0;
+}
