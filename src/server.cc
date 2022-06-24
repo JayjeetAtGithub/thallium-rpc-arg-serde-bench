9b56ac9e47aef8089a4a49201f9681c16d65ff08
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-24 14:10:11 -0700

    fix server code

--- src/server.cc
+++ src/server.cc
@@ -16,21 +16,3 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-#include <iostream>
-#include <thallium.hpp>
-
-namespace tl = thallium;
-
-void sum(const tl::request& req, int x, int y) {
-    std::cout << "Computing " << x << "+" << y << std::endl;
-    req.respond(x+y);
-}
-
-int main(int argc, char** argv) {
-
-    tl::engine myEngine("tcp://127.0.0.1:1234", THALLIUM_SERVER_MODE);
-    std::cout << "Server running at address " << myEngine.self() << std::endl;
-    myEngine.define("sum", sum);
-
-    return 0;
-}
