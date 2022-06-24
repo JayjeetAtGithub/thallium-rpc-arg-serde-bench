f118d9c1f774b225363209a78f930bf6b2ad29b0
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-24 15:02:09 -0700

    add rpc

--- src/client.cc
+++ src/client.cc
@@ -27,11 +27,10 @@ int main(int argc, char** argv) {
     }
 
     int num_rows = 1000;
-    int num_cols = 17;
-    std::vector<int> types{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
-    std::vector<int64_t> data_buff_sizes{14564, 36366, 57755, 44833, 33323, 44445, 55555, 99999, 647474, 34635, 3848, 485757, 47474, 92784, 48489, 29847, 38474};
-    std::vector<int64_t> offset_buff_sizes{143564, 31266, 57755, 44233, 33063, 41445, 35255, 99998, 637474, 34335, 38480, 485457, 17474, 72784, 43489, 29347, 38174};;    
-
+    int num_cols = 2;
+    std::vector<int> types{1,2};
+    std::vector<int64_t> data_buff_sizes{1000, 3000};
+    std::vector<int64_t> offset_buff_sizes{5000, 6000};    
 
     tl::engine myEngine("verbs", THALLIUM_CLIENT_MODE);
     
