1d1d09e4bc910425918ebcbacbdd97d8f4bf7d0b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-06-24 14:19:55 -0700

    send arguments

--- src/client.cc
+++ src/client.cc
@@ -8,10 +8,18 @@ int main(int argc, char** argv) {
         std::cerr << "Usage: " << argv[0] << " <address>" << std::endl;
         exit(0);
     }
+
+    int64_t num_rows = 1024*1024;
+    int64_t num_cols = 17;
+    std::vector<int> types{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+    std::vector<int64_t> data_buff_sizes{14564, 36366, 57755, 44833, 33323, 44445, 55555, 99999, 647474, 34635, 3848, 485757, 47474, 92784, 48489, 29847, 38474};
+    std::vector<int64_t> offset_buff_sizes{143564, 31266, 57755, 44233, 33063, 41445, 35255, 99998, 637474, 34335, 38480, 485457, 17474, 72784, 43489, 29347, 38174};;    
+
+
     tl::engine myEngine("verbs", THALLIUM_CLIENT_MODE);
     tl::remote_procedure sum = myEngine.define("sum");
     tl::endpoint server = myEngine.lookup(argv[1]);
-    int ret = sum.on(server)(42,63);
+    int ret = sum.on(server)(num_rows, num_cols, types, data_buff_sizes, offset_buff_sizes);
     std::cout << "Server answered " << ret << std::endl;
 
     return 0;
--- src/server.cc
+++ src/server.cc
@@ -3,7 +3,7 @@
 
 namespace tl = thallium;
 
-void sum(const tl::request& req, int x, int y) {
+void sum(const tl::request& req, int a, int b, std::vector<int> c, std::vector<int64_t> d, std::vector<int64_t> e) {
     std::cout << "Computing " << x << "+" << y << std::endl;
     req.respond(x+y);
 }
